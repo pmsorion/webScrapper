0997ac663ec7d691313a545ead8ab44659051c47
Francisco Garcia <garciafran@hotmail.com>
2020-10-01 16:26:49 -0500

    Persistiendo la informacion scrapeada

--- main.py
+++ main.py
@@ -1,20 +1,80 @@
 import argparse
+import csv
+import datetime
 import logging
 logging.basicConfig(level=logging.INFO)
+import re
+
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 
 import news_page_objects as news
 from common import config
 
+is_well_formed_link = re.compile(r'^https?://.+/.+$') #https://example.com/hello
+is_root_path = re.compile(r'^/.+$') #/some-text
 logger = logging.getLogger(__name__)
 
+
 def _news_scraper(news_site_uid):
-    host = config()["news_sites"][news_site_uid]['url']
+    host = config()['news_sites'][news_site_uid]['url']
 
     logging.info('Beginning scraper for {}'.format(host))
+    logging.info('Finding links in homepage...')
+
     homepage = news.HomePage(news_site_uid, host)
 
+    articles = []
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info('Article Fetched')
+            articles.append(article)
+            
+    _save_articles(news_site_uid, articles)
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(news_site_uid=news_site_uid, datetime=now)
+    
+
+    with open(out_file_name, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
+
+
+    #print(len(article))
+
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info('Start feching article at {}'.format(link))
+
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article', exc_info=False)
+
+    if  article and not article.body:
+        logger.warning('Invalid article. There is no body')
+        return None
+
+    return article
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return '{host}{uri}'.format(host=host, uri=link)
+    else:
+        return '{host}/{uri}'.format(host=host, uri=link)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
