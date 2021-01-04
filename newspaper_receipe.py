57c7c54d7ea80d6fa328b710e7a819dc4d62523f
Francisco Garcia <garciafran@hotmail.com>
2021-01-04 11:40:36 -0500

    Add Data Enrichment

--- newspaper_receipe.py
+++ newspaper_receipe.py
@@ -1,10 +1,13 @@
 import argparse
+import hashlib
 import logging
 logging.basicConfig(level=logging.INFO)
 from urllib.parse import urlparse
-
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
 
+stop_words = set(stopwords.words('spanish'))
 logger = logging.getLogger(__name__)
 
 def main(filename):
@@ -14,6 +17,11 @@ def main(filename):
     newspapaer_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspapaer_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
+    df = _generate_uid_for_rows(df)
+    df = _remove_new_lines_from_body(df)
+    df['n_tokens_title'] = _data_enrichement(df, 'title')
+    df['n_tokens_body'] = _data_enrichement(df, 'body')
 
     return df
 
@@ -37,9 +45,58 @@ def _add_newspaper_uid_column(df, newspapaer_uid):
 
 def _extract_host(df):
     logger.info('Extracting host from urls')
-    df[host] = df['url'].apply(lambda url: urlparse(url).netloc)
+    df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
 
     return df
+
+def _fill_missing_titles(df):
+    logger.info('Filling missing tittles')
+    missing_titles_mask = df['title'].isna()
+
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
+                        .applymap(lambda title: title.split('-'))
+                        .applymap(lambda title_word_list: ' '.join(title_word_list))
+                    )
+
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+
+    return df
+
+def _generate_uid_for_rows(df):
+    logger.info('Generate uids for each row')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
+            .apply(lambda hash_object: hash_object.hexdigest())
+            )
+
+    df['uid'] = uids
+
+    return df.set_index('uid')
+
+def _remove_new_lines_from_body(df):
+    logger.info('Remove new lines from body')
+
+    stripped_body = (df
+                        .apply(lambda row: row['body'], axis=1)
+                        .apply(lambda body: list(body))
+                        .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ''), letters)))
+                        .apply(lambda letters: ''.join(letters))
+                    )
+
+    df['body'] = stripped_body
+
+    return df
+
+def _data_enrichement(df, column_name):
+    return (df
+                .dropna()
+                .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
+                .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+                .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
+                .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+                .apply(lambda valid_word_list: len(valid_word_list))
+    )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -47,6 +104,6 @@ if __name__ == '__main__':
                         help='The path to the dirty data',
                         type=str)
 
-    arg = parser.parse_args()
+    args = parser.parse_args()
     df = main(args.filename)
     print(df)
